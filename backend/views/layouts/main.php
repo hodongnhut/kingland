c7515897d7628cd4182b50ba8519c3c57e1abcd6
Đông Nhựt <hodongnhut@gmail.com>
2025-07-06 17:37:05 +0700

    update

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -29,7 +29,7 @@ AppAsset::register($this);
     <aside class="flex flex-col items-start py-4">
         <div class="mb-8 px-3">
             <div class="h-10 w-10 bg-yellow-500 rounded-lg flex items-center justify-center text-white text-xl font-bold overflow-hidden">
-                <img src="img/logo.png" alt="King Land" class="h-full w-full object-contain">
+            <img src="<?= Yii::$app->request->baseUrl ?>/img/logo.png" alt="King Land" class="h-full w-full object-contain">
             </div>
         </div>
         <nav class="flex flex-col space-y-2 w-full">
