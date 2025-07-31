95d94d0b250d7bb4e273b0e9478527f07b4bd7c6
Leo <hodongnhut@gmail.com>
2025-07-31 12:51:33 +0700

    update

--- backend/views/property/index.php
+++ backend/views/property/index.php
@@ -415,7 +415,7 @@ $this->registerJsFile('https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/s
             [
                 'label' => 'Lưu',
                 'contentOptions' => ['class' => 'px-6 py-4  text-sm text-gray-900'],
-                'headerOptions' => ['class' => 'px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider  bg-yellow-500 hover:bg-yellow-600 text-white hidden md:table-cel'],
+                'headerOptions' => ['class' => 'px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider  bg-yellow-500 hover:bg-yellow-600 text-white'],
                 'format' => 'raw',
                 'value' => function ($model) {
                     $propertyId = $model->property_id; 
