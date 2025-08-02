f8e171c99138e5b4e522ca37f192c174ab785de8
Leo <hodongnhut@gmail.com>
2025-08-02 13:49:19 +0700

    update

--- common/models/Properties.php
+++ common/models/Properties.php
@@ -125,8 +125,11 @@ class Properties extends \yii\db\ActiveRecord
     public function updateChart($event) {
         UserActivities::logActivity(Yii::$app->user->id, 'add_new');
         $model = $event->sender;
-        $model->new_district = $this->findNewDistrict($model->city, $model->district_county, $model->ward_commune);
-        $model->save();
+        if (!empty($model->new_district)) {
+            $model->new_district = $this->findNewDistrict($model->city, $model->district_county, $model->ward_commune);
+            $model->save();
+        }
+        
         return;
     }
 
