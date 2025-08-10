a6e068dc0ab066647cd718f1a5104c2e106584ac
Leo <hodongnhut@gmail.com>
2025-08-10 08:45:50 +0700

    update

--- backend/views/property/index.php
+++ backend/views/property/index.php
@@ -328,10 +328,18 @@ $this->registerJsFile('https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/s
             'headerOptions' => ['class' => 'px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider  bg-yellow-500 hover:bg-yellow-600 text-white hidden md:table-cell'],
             'format' => 'raw',
             'value' => function ($model) {
-                $price = number_format($model->price / 1e9, 2) . ' Tỷ ' . $model->currencies->code;
-                $pricePerM2 = (!empty($model->area_total) && $model->area_total > 0) 
-                    ? number_format($model->price / $model->area_total / 1e6, 0) . ' Triệu/m2' 
-                    : '-';
+                if ($model->listing_types_id == 2) {
+                    $price = Yii::$app->formatter->asCurrency($model->price, 'VND');
+                    $pricePerM2 = (!empty($model->area_total) && $model->area_total > 0) 
+                        ? number_format($model->price / $model->area_total / 1e6, 0) . ' Triệu/m2' 
+                        : '-';
+                } else {
+                    $price = number_format($model->price / 1e9, 2) . ' Tỷ ' . $model->currencies->code;
+                    $pricePerM2 = (!empty($model->area_total) && $model->area_total > 0) 
+                        ? number_format($model->price / $model->area_total / 1e6, 0) . ' Triệu/m2' 
+                        : '-';
+                }
+               
                 return Html::tag('div', $price, ['class' => 'font-semibold text-red-600']) .
                     Html::tag('div', '-' . $pricePerM2, ['class' => 'text-xs text-gray-600']);
             },
