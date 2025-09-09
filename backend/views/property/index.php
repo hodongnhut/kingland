77c14d4cde52a0faf79feb76aceb39a8e0a56db5
Leo <hodongnhut@gmail.com>
2025-09-09 13:47:23 +0700

    updateupdate

--- backend/views/property/index.php
+++ backend/views/property/index.php
@@ -359,7 +359,7 @@ $this->registerJsFile('https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/s
         [
             'label' => 'Kết Cấu',
             'contentOptions' => ['class' => 'px-6 py-4 whitespace-nowrap text-sm text-gray-900 hidden md:table-cell'],
-            'headerOptions' => ['class' => 'px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider  bg-yellow-500 hover:bg-yellow-600 text-white hidden md:table-cell text-[13.5px]'],
+            'headerOptions' => ['class' => 'px-6 py-3 text-left text-xs font-medium text-gray-500 capitalize tracking-wider  bg-yellow-500 hover:bg-yellow-600 text-white hidden md:table-cell text-[13.5px]'],
             'value' => function ($model) { 
                 if ($model->num_floors > 0) {
                     return $model->num_floors . ' tầng'; 
