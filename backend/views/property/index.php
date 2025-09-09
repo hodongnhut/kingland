705c00aefef329576fb0b5a5ceba453a4f1276f5
Leo <hodongnhut@gmail.com>
2025-09-09 13:48:09 +0700

    updateupdate

--- backend/views/property/index.php
+++ backend/views/property/index.php
@@ -487,7 +487,7 @@ $this->registerJsFile('https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/s
             'attribute' => 'status_review',
             'label' => 'Duyệt Tin',
             'contentOptions' => ['class' => 'px-6 py-4 whitespace-nowrap text-sm text-gray-900 hidden md:table-cell'],
-            'headerOptions' => ['class' => 'px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-yellow-500 hover:bg-yellow-600 text-white'],
+            'headerOptions' => ['class' => 'px-6 py-3 text-left text-xs font-medium text-gray-500 capitalize tracking-wider bg-yellow-500 hover:bg-yellow-600 text-white'],
             'format' => 'raw',
             'value' => function ($model) {
                 $status = $model->status_review ?? 0;
